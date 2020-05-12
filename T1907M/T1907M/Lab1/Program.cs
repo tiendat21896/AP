22e9da776f288ffaa22ea0e67232ee0cfdaf7989
Đạt Nguyễn <53225576+tiendat21896@users.noreply.github.com>
2020-05-12 11:22:05 +0700

    Add files via upload

---
+++ T1907M/T1907M/Lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T1907M.Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            findFibonaci();
+        }
+
+        static void findFibonaci()
+        {
+            int n;
+            int fb = 0;
+            int fb1 = 1;
+            int fb2 = 1;
+            Console.WriteLine("Nhap vao n:");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (fb1+fb2<n)
+            {
+                fb = fb1 + fb2;
+                fb1 = fb2;
+                fb2 = fb;
+            }
+            Console.WriteLine("So fibonaci < n = "+fb);
+        }
+    }
+}
